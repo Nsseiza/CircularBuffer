d83d3e51df351d4b1c0b56782f43d91e06d7bfa2
Pedja <predrag.seizovic@panonit.com>
2019-12-11 17:59:39 +0100

    Update circular_buffer.c file

--- circular_buffer.c
+++ circular_buffer.c
@@ -28,8 +28,8 @@ int Is_empty(buffer_struct* buff)
 
 int put(buffer_struct* buff, int data)
 {
-	if(Is_full())
-		break;
+	if(Is_full(buff))
+		return 0;
 	else
 	{
 		*(buff->write) = data;
@@ -38,4 +38,54 @@ int put(buffer_struct* buff, int data)
 		if(buff->write == &(buff->buffer[BUFF_SIZE]))
 			buff->write = buff->buffer;
 	}
+	
+	return 1;
 }
+
+int get(buffer_struct* buff)
+{
+	if(Is_empty(buff))
+		return 0;
+	
+	int data = *(buff->read);
+	buff->read++;
+
+	if(buff->read == &(buff->byffer[BUFF_SIZE]))
+		buff->read = buffer;
+
+	return data;	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
