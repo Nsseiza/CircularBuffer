9ecc5f46d4fb20b4043df8aa917b217fc04d06f5
Pedja <predrag.seizovic@panonit.com>
2019-12-11 17:11:45 +0100

    Add functions(Init, Is_full, Is_empty) to circular_buffer(.c/.h) file

---
+++ circular_buffer.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "circular_buffer.h"
+
+int Init_buffer(buffer_struct* buff)
+{
+	buff->write = buff->buffer;
+	buff->read = buff->buffer;
+}
+
+int Is_full(buffer_struct* buff)
+{
+	if((buff->write+1) == buff->read)
+		return 1;
+	if((buff->read == &(buff->buffer[0])) && (buff->write == &(buff->buffer[BUFF_SIZE-1])))
+		return 1;
+	
+	return 0;
+}
+
+int Is_empty(buffer_struct* buff)
+{
+	if(buff->read == buff->write)
+		return 1;
+	
+	return 0;
+}
---
+++ circular_buffer.h
@@ -0,0 +1,20 @@
+#ifndef CURCULARBUFFER_H_
+#define CIRCULARBUFFER_H_
+
+#define BUF_SIZE 11
+
+typedef struct buffer_struct
+{
+	int buffer[BUFF_SIZE];
+	buffer_struct* write;
+	buffer_struct* read;
+
+}buffer_struct;
+
+int Init_buffer(buffer_struct* buff);
+
+int Is_full(buffer_struct* buff);
+
+int Is_empty(buffer_struct* buff);
+
+#endif
