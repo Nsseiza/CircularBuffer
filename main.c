36aed6d2638139604eaf5c5f2da64db68fd185d0
Pedja <predrag.seizovic@panonit.com>
2019-12-12 11:55:28 +0100

    Update main.c file

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "circular_buffer.h"
+
 int main(int argc, char* argv[])
 {
+	buffer_struct buff_1;
+
+	Init_buffer(&buff_1);
+	
+	put(&buff_1, 5);
+	put(&buff_1, 10);
+	
+	printf("\n%d", get(&buff_1));
 	
 	rerturn 0;
 }
