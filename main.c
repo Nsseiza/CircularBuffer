3c72007fac2b12232f13d1005090c00c75ec4e22
Pedja <predrag.seizovic@panonit.com>
2019-12-12 16:49:35 +0100

    Fix bugs in cirkular_buffer.c file

--- main.c
+++ main.c
@@ -9,8 +9,18 @@ int main(int argc, char* argv[])
 
 	Init_buffer(&buff_1);
 	
+	put(&buff_1, 1);
+	put(&buff_1, 2);
+	put(&buff_1, 3);
+	put(&buff_1, 4);
 	put(&buff_1, 5);
+	put(&buff_1, 6);
+	put(&buff_1, 7);
+	put(&buff_1, 8);
+	put(&buff_1, 9);
 	put(&buff_1, 10);
+	put(&buff_1, 11);
+	put(&buff_1, 12);
 	
 	printf("\n%d\n", get(&buff_1));
 	printf("\n%d\n", get(&buff_1));
